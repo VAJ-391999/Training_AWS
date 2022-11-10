c38b2dc9309a1ec318f89a69f68cfb26030a382a
VAJ-391999 <venishachauhan1234@gmail.com>
2022-11-10 10:37:21 +0530

    Memorization

--- sky-shopping/src/app/admin/dashboard/chart/chart.component.ts
+++ sky-shopping/src/app/admin/dashboard/chart/chart.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import * as memoizee from 'memoizee';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  styleUrls: ['./chart.component.css'],
 })
 export class ChartComponent implements OnInit {
+  framework: string = 'Angular';
+  count: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  getTitle = memoizee((framework: string) => {
+    console.log('Get Title');
+    return framework.toUpperCase();
+  });
+
+  changeFramework = () => {
+    if (this.framework === 'Angular') {
+      this.framework = 'React';
+    } else {
+      this.framework = 'Angular';
+    }
+  };
+
+  increaseCount = () => {
+    this.count++;
+  };
 }
